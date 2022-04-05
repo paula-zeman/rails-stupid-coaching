1d5cbc84d3c6e2a4f29294b98e57fabed86036b1
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-05 17:41:52 +0200

    Built stupid coach yay

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @message = params[:input]
+
+    if @message.end_with?("?")
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @message.downcase == 'i am going to work'
+      @answer = 'Great!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
